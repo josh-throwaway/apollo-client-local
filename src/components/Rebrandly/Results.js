245f29e0e9db5cba65e68dd848788d92863a0d79
Josh Thurston <Joshthurston92@gmail.com>
2022-03-13 00:11:28 -0700

    styling for results

--- src/components/Rebrandly/Results.js
+++ src/components/Rebrandly/Results.js
@@ -1,8 +1,47 @@
+import React, { useState, useEffect } from "react";
 import {useQuery, gql} from '@apollo/client';
 import { Grid, Typography } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import {ArrowRightAlt , ArrowDownward} from '@material-ui/icons';
+
+
+const useStyles = makeStyles((theme) => ({
+
+  result1: {
+    border: "1px solid black"
+  },
+  result0: {
+    backgroundColor: "#c5c5c5",
+    borderLeft: "1px solid black",
+    borderRight: "1px solid black"
+  },
+  centered: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+}));
+
+const getIsMobile = () => window.innerWidth <= 959;
 
 
 export default function Results() {
+  const classes = useStyles();
+  const [isMobile, setIsMobile] = useState(getIsMobile());
+
+  useEffect(() => {
+    const onResize = () => {
+        setIsMobile(getIsMobile());
+    }
+
+    window.addEventListener("resize", onResize);
+
+    return () => {
+        window.removeEventListener("resize", onResize);
+    }
+}, []);
+
+
   const { loading, error, data } = useQuery(gql`
     {
       allLinks {
@@ -18,13 +57,16 @@ export default function Results() {
 
   return (
     <Grid container>
-      {data.allLinks.map(({ url, slug, id }) => (
-        <Grid container key={id}>
-          <Grid item xs={12} md={8}>          
+      {data.allLinks.map(({ url, slug, id }, index) => (
+        <Grid container key={index} className={classes[`result${index%2}`]}>
+          <Grid item xs={12} md={5} className={classes.centered}>          
             <Typography>{url}</Typography>
           </Grid>
-          <Grid item xs={12} md={4}> 
-            <Typography>-> https://hdwy.link/{slug}</Typography>
+          <Grid item xs={12} md={2} className={classes.centered}>
+            {!isMobile ? <ArrowRightAlt /> : <ArrowDownward />}
+          </Grid>
+          <Grid item xs={12} md={5} className={classes.centered}> 
+            <Typography>https://hdwy.link/{slug}</Typography>
           </Grid>
         </Grid>
       ))}
